db6ec2cdbef509030ab0745b1eb7c12b0be575f7
hrhazelwood <hrhazelwood@crimson.ua.edu>
2024-09-23 03:18:07 -0500

    initial setup

---
+++ Fall2024-Assignment1-hrhazelwood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assignment1_hrhazelwood.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
